7a410b8c2e798a9a8e82488679a16144c61fae88
Robsiorek <robert@viacamp.pl>
2025-01-11 01:42:41 +0100

    changes #1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: ['./index.html', './src/**/*.{html,js}'], // Dodano ścieżkę do index.html
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1280px', // Nadal 1280px dla ekranów 1536px+
+      },
+    },
+    extend: {
+      fontFamily: {
+        lato: ['Lato', 'Arial', 'sans-serif'], // Zmieniamy domyślną czcionkę "sans"
+      },
+    },
   },
   plugins: [],
 };
